3ad1b806290858ceffb45b30e339a23d323fbb35
Mieschendahl <mieschendahl.leonardo@gmail.com>
2026-06-09 12:03:13 +0200

    updated basic layout

---
+++ src/app/pages/main/main.ts
@@ -0,0 +1,30 @@
+import { Component, signal, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.css',
+})
+export class Main {
+  private router = inject(Router);
+
+  username = signal('');
+
+  setUsername(value: string) {
+    this.username.set(value);
+  }
+
+  play() {
+    const user = this.username().trim();
+
+    if (!user) {
+      return;
+    }
+
+    this.router.navigate(['/play'], {
+      queryParams: { user },
+    });
+  }
+}
