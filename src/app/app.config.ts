6c64ca519029878edd9aa8bc35e7ca7fe2151cda
Mieschendahl <mieschendahl.leonardo@gmail.com>
2026-06-12 14:05:41 +0200

    updated code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: false
-        }
-      }
+      unstyled: true
     })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SocketService } from './services/socket/socket.service';
+import { Background } from './components/background/background';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Background],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
